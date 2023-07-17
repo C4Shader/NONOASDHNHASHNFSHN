0d9465d9d91f9721bc65a64eebac6c7e9d1b4805
rxbby <111032913+C4Shader@users.noreply.github.com>
2023-07-17 05:32:08 -0500

    Update C4.lua

--- C4.lua
+++ C4.lua
@@ -98,7 +98,7 @@ local themeStyles = {
         TextColor = Color3.fromRGB(255, 255, 255),
         ElementColor = Color3.fromRGB(52, 74, 95)
     },
-    RobbyTheme = {
+    Sentinel = {
         SchemeColor = Color3.fromRGB(93, 0, 255),
         Background = Color3.fromRGB(16, 16, 16),
         Header = Color3.fromRGB(24, 24, 24),
