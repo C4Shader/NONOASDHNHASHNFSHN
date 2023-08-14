b8584dbc9a5ac7d2f55f691cb3eb9e262a5ccbca
rxbby <111032913+C4Shader@users.noreply.github.com>
2023-08-14 02:26:08 -0500

    Update C4.lua

--- C4.lua
+++ C4.lua
@@ -99,7 +99,7 @@ local themeStyles = {
         ElementColor = Color3.fromRGB(52, 74, 95)
     },
     Sentinel = {
-        SchemeColor = Color3.fromRGB(72, 0, 255),
+        SchemeColor = Color3.fromRGB(65, 0, 255),
         Background = Color3.fromRGB(16, 16, 16),
         Header = Color3.fromRGB(24, 24, 24),
         TextColor = Color3.fromRGB(255,255,255), 
@@ -113,11 +113,11 @@ local themeStyles = {
         ElementColor = Color3.fromRGB(24, 24, 24)
     },
     Serpent = {
-        SchemeColor = Color3.fromRGB(0, 166, 58),
-        Background = Color3.fromRGB(31, 41, 43),
-        Header = Color3.fromRGB(22, 29, 31),
-        TextColor = Color3.fromRGB(255,255,255),
-        ElementColor = Color3.fromRGB(22, 29, 31)
+        SchemeColor = Color3.fromRGB(0, 255, 166),
+        Background = Color3.fromRGB(16, 16, 16),
+        Header = Color3.fromRGB(24, 24, 24),
+        TextColor = Color3.fromRGB(255,255,255), 
+        ElementColor = Color3.fromRGB(24, 24, 24)
     }
 }
 local oldTheme = ""
